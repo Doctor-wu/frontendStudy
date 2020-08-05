0cb6b7821de2909a61a0c0b5980cc1c6785e3024
Doctorwu <wuzhouchun@qq.com>
2020-08-05 14:49:11 +0800

    堆栈内存_06 闭包和作用域_01

--- js底层，核心，基础/preview/数据类型和堆栈内存/example.js
+++ js底层，核心，基础/preview/数据类型和堆栈内存/example.js
@@ -1,11 +1,18 @@
-var x = [12, 23];
+// var x = [12, 23];
 
-function fn(y) {
-    y[0] = 100;
-    y = [100];
-    y[1] = 200;
-    console.log(y); // [100, 200]
-}
+// function fn(y) {
+//     y[0] = 100;
+//     y = [100];
+//     y[1] = 200;
+//     console.log(y); // [100, 200]
+// }
 
-fn(x);
-console.log(x); // [100, 23]
+// fn(x);
+// console.log(x); // [100, 23]
+
+// var fn = 123;
+
+// var fn = function fn() {
+//     console.log('fn');
+// }
+// console.log(fn);
