8539e67bfde9c9e83cefd7c1114bec9e4ae112e5
Doctorwu <wuzhouchun@qq.com>
2020-09-08 07:53:40 +0800

    debounce进阶

--- js底层，核心，基础/preview/闭包和作用域链/函数的节流和防抖/debounce.js
+++ js底层，核心，基础/preview/闭包和作用域链/函数的节流和防抖/debounce.js
@@ -1,18 +1,42 @@
-function debounce(func, delay = 300, immediately = false) {
-    let timer = null;
+// function debounce(func, delay = 300, immediately = false) {
+//     let timer = null;
+//
+//     return function anonymous(...params) {
+//         let now = immediately && !timer;
+//         clearTimeout(timer);
+//
+//         timer = setTimeout(() => {
+//             timer = null;
+//             !immediately ? func.apply(this, params) : null;
+//         }, delay)
+//
+//         now ? func.apply(this, params) : null;
+//     }
+// }
 
-    return function anonymous(...params) {
-        let now = immediately && !timer;
-        clearTimeout(timer);
 
-        timer = setTimeout(() => {
-            timer = null;
-            !immediately ? func.apply(this, params) : null;
-        }, delay)
-
-        now ? func.apply(this, params) : null;
+// 利用惰性思想，immediately判断了一次之后就不用再次判断了
+function debounce(func, delay = 500, immediately = false) {
+    let timer;
+    if (!immediately) {
+        return function(...args) {
+            timer && clearTimeout(timer);
+            timer = setTimeout(() => {
+                func.apply(this, args);
+                timer = null;
+            }, delay);
+        }
+    } else {
+        return function(...args) {
+            timer && clearTimeout(timer);
+            !timer && func.apply(this, args);
+            timer = setTimeout(() => {
+                timer = null;
+            }, delay);
+        }
     }
 }
+
 
 
 function handle() {
@@ -20,4 +44,4 @@ function handle() {
 }
 
 
-submit.onclick = debounce(handle, 500, true)
+submit.onclick = debounce(handle, 500, true);
