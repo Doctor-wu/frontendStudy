6c98377d46de1dacea2311560e02c992882471eb
Doctorwu <wuzhouchun@qq.com>
2020-09-08 20:50:55 +0800

    less&sass&bootstrap使用和原理

---
+++ CSS高级/demos/less和sass实践/compiler.js
@@ -0,0 +1,37 @@
+const fs = require.call(this, 'fs');
+const path = require.call(this, 'path');
+const cmd = require('node-cmd');
+
+function load(dir, cb) {
+    const url = path.resolve(__dirname, dir);
+    console.log(url)
+    const files = fs.readdirSync(url);
+    files.forEach(filename => {
+        cb(filename);
+    })
+}
+
+function initLESS() {
+    load('./less/less', (filename) => {
+        if (filename.startsWith("_nc")) return;
+        console.log(filename);
+        const origin = path.resolve(__dirname, 'less', `less/${filename}`);
+        const target = path.resolve(__dirname, 'less', `target/${filename.replace('less','css')}`);
+        // console.log(`${origin} ${target}`);
+        cmd.run(`lessc ${origin} ${target}`);
+    })
+}
+
+function initSASS() {
+    load('./sass/sass', (filename) => {
+        if (filename.startsWith("_nc")) return;
+        console.log(filename);
+        const origin = path.resolve(__dirname, 'sass', `sass/${filename}`);
+        const target = path.resolve(__dirname, 'sass', `target/${filename.replace('scss','css')}`);
+        // console.log(`${origin} ${target}`);
+        cmd.run(`node-sass --output-style nested  ${origin} > ${target}`);
+    })
+}
+let lang = process.argv.find(i => i.startsWith('lang')).split('=')[1];
+console.log(`init${lang.toUpperCase()}`);
+eval(`init${lang.toUpperCase()}()`);
---
+++ CSS高级/demos/less和sass实践/note.md
@@ -0,0 +1,19 @@
+## less实践
+
+
+
+### less的功能
+
+变量：@
+
+嵌套 Nesting
+
+混合 Mixins
+
+扩展 Extend
+
+作用域 Scope
+
+循环 Loop
+
+导入 Importing
