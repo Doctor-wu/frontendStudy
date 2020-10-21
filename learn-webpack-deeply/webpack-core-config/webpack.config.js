db77698fc8abd99f4e9240c4ae857ff9c4956a35
Doctorwu <wuzhouchun@qq.com>
2020-10-21 15:19:27 +0800

    little

--- learn-webpack-deeply/webpack-core-config/webpack.config.js
+++ learn-webpack-deeply/webpack-core-config/webpack.config.js
@@ -7,7 +7,7 @@ const commonConfig = require("./webpack.config.base.js");
 const prodConfig = require("./webpack.config.prod.js");
 const devConfig = require("./webpack.config.dev.js");
 
-console.log(process.env.NODE_ENV, isProd, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+console.log(process.env.NODE_ENV, isProd);
 
 
 module.exports = () => {
