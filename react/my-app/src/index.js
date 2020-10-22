8f436b96a2acdae67e2ceb7e5960f16c33c5c0c1
Doctorwu <wuzhouchun@qq.com>
2020-10-22 13:47:07 +0800

    JSX

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,15 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import styles from "./index.module.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const myName = "Doctorwu";
+const profile = {
+  languages: ["javascript", "typescript"],
+  age: 20,
+  sex: "male",
+};
+
+const formatter = function (profile) {
+  return (
+    <div>
+      <strong>languages</strong>
+      <ul>
+        {profile.languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <strong>Sex</strong>
+      <ul>
+        <li>{profile.sex}</li>
+      </ul>
+    </div>
+  );
+};
+
+const jsx = (
+  //   <React.StrictMode>
+  //     <App />
+  //   </React.StrictMode>
+  <div>
+    <h2 className={styles.name}>My name is {myName}</h2>
+    {formatter(profile)}
+    <h3>
+      {myName}是{profile.sex === "male" ? "男" : "女"}的
+    </h3>
+    <hr />
+  </div>
 );
+
+ReactDOM.render(jsx, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ react/preview/init.md
@@ -0,0 +1,10 @@
+## React-init
+
+### 创建一个React应用
+
+```bash
+npx create-react-app
+yarn start;
+yarn build
+```
+
