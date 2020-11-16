8ef80b964c46f54d9690a968b100134a9ee9ff5a
Doctorwu <wuzhouchun@qq.com>
2020-11-16 19:02:33 +0800

    react-ts

--- typescript/1.js
+++ typescript/1.js
@@ -1,2 +1,8 @@
 "use strict";
 var dtname = "doctorwu";
+var Person = /** @class */ (function () {
+    function Person(name) {
+        console.log(name);
+    }
+    return Person;
+}());
--- typescript/1.ts
+++ typescript/1.ts
@@ -1,1 +1,7 @@
 let dtname:string = "doctorwu";
+class Person {
+    constructor(name:string){
+        console.log(name);
+        
+    }
+}
