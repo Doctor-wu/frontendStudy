866c163b2a7029353717834b1a2f2f699071fbc8
Doctorwu <wuzhouchun@qq.com>
2020-11-29 13:18:04 +0800

    单例模式

--- 设计模式/entity/JS/单例模式.js
+++ 设计模式/entity/JS/单例模式.js
@@ -1,1 +1,99 @@
 "use strict";
+/**
+ * 普通单例
+ *  缺点是类的使用者必须知道这是一个单例模式的类，并且主动调用获取实例的方法
+ * */
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var NormalSingle;
+(function (NormalSingle) {
+    var Single = /** @class */ (function () {
+        function Single(name) {
+            this.name = name;
+        }
+        Single.getInstance = (function () {
+            var instance; // 闭包
+            return function (name) {
+                if (!instance) {
+                    instance = new Single(name);
+                }
+                return instance;
+            };
+        })();
+        return Single;
+    }());
+    NormalSingle.Single = Single;
+})(NormalSingle || (NormalSingle = {}));
+var s1 = NormalSingle.Single.getInstance("doctorwu");
+var s2 = NormalSingle.Single.getInstance("doctorwu666");
+console.log(s1, s2, s1 === s2);
+/**
+ * 透明单例模式
+ *  类的使用者正常使用即可
+ * */
+var TransparentSingle;
+(function (TransparentSingle) {
+    var Single = /** @class */ (function () {
+        function Single(name) {
+            if (!Single.instance) {
+                this.name = name;
+                Single.instance = this;
+            }
+            return Single.instance;
+        }
+        Single.instance = undefined;
+        return Single;
+    }());
+    TransparentSingle.Single = Single;
+})(TransparentSingle || (TransparentSingle = {}));
+var s3 = new TransparentSingle.Single("doctorwu666");
+var s4 = new TransparentSingle.Single("doctorwu");
+console.log(s3, s4, s3 === s4);
+/***
+ * 创建单例函数的函数
+ * @param Cstr
+ * @constructor
+ */
+function CreateSingle(Cstr) {
+    var instance;
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        if (!instance) {
+            instance = new (Cstr.bind.apply(Cstr, __spreadArrays([void 0], args)))();
+        }
+        return instance;
+    };
+}
+var My = /** @class */ (function () {
+    function My(name) {
+        this.name = name;
+    }
+    My.prototype.getName = function () {
+        return this.name;
+    };
+    return My;
+}());
+var You = /** @class */ (function () {
+    function You(age) {
+        this.age = age;
+    }
+    You.prototype.getAge = function () {
+        return this.age;
+    };
+    return You;
+}());
+var mySingle = CreateSingle(My);
+var youSingle = CreateSingle(You);
+console.log(mySingle("dtwu"), mySingle("doctorwu"), mySingle("dtwu") === mySingle("doctorwu"));
+console.log(youSingle("doctorwu"), youSingle("dtwu"), youSingle("dtwu") === youSingle("doctorwu"));
+console.log(youSingle("dtwu212").getAge());
+console.log(mySingle("dtwu2432").getName());
---
+++ 设计模式/entity/单例模式.ts
@@ -0,0 +1,119 @@
+/**
+ * 普通单例
+ *  缺点是类的使用者必须知道这是一个单例模式的类，并且主动调用获取实例的方法
+ * */
+
+namespace NormalSingle {
+    export class Single {
+        public name: string;
+
+        constructor(name: string) {
+            this.name = name;
+        }
+
+        static getInstance: (name: string) => Single = (function () {
+            let instance: Single; // 闭包
+
+
+            return function (name: string) {
+                if (!instance) {
+                    instance = new Single(name);
+                }
+
+                return instance;
+            };
+        })()
+    }
+}
+
+let s1 = NormalSingle.Single.getInstance("doctorwu");
+let s2 = NormalSingle.Single.getInstance("doctorwu666");
+
+console.log(s1, s2, s1 === s2);
+
+
+/**
+ * 透明单例模式
+ *  类的使用者正常使用即可
+ * */
+
+
+namespace TransparentSingle {
+
+    export class Single {
+        public name?: string;
+        static instance?: Single = undefined;
+
+        constructor(name: string) {
+            if (!Single.instance) {
+                this.name = name;
+                Single.instance = this;
+            }
+
+            return Single.instance;
+        }
+    }
+}
+
+
+let s3 = new TransparentSingle.Single("doctorwu666");
+let s4 = new TransparentSingle.Single("doctorwu");
+
+console.log(s3, s4, s3 === s4);
+
+
+/***
+ * 创建单例函数的函数
+ * @param Cstr
+ * @constructor
+ */
+
+function CreateSingle<T>(Cstr: ICstr<T>) {
+    let instance: T;
+    return function (...args: Array<any>) {
+        if (!instance) {
+            instance = new Cstr(...args);
+        }
+        return instance;
+    }
+}
+
+export interface Constructable<T> {
+
+    new(...args: any[]): T
+}
+
+
+interface ICstr<T> {
+    new(...args: Array<any>): T;
+}
+
+class My {
+    public name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    getName() {
+        return this.name;
+    }
+}
+
+class You {
+    constructor(public age: number) {
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+}
+
+let mySingle = CreateSingle(My);
+let youSingle = CreateSingle(You);
+console.log(mySingle("dtwu"), mySingle("doctorwu"), mySingle("dtwu") === mySingle("doctorwu"));
+console.log(youSingle("doctorwu"), youSingle("dtwu"), youSingle("dtwu") === youSingle("doctorwu"));
+console.log(youSingle("dtwu212").getAge());
+console.log(mySingle("dtwu2432").getName());
+
