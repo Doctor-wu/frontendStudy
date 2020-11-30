57caf3f6f9665ca9b96fffe3012317f4d79565a3
Doctorwu <wuzhouchun@qq.com>
2020-11-30 13:58:39 +0800

    代理模式

---
+++ 设计模式/entity/图片懒加载-代理模式/server.ts
@@ -0,0 +1,45 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import Static from "koa-static";
+import path from "path";
+import fs from "fs";
+
+const app = new Koa();
+
+app.use(Static(path.join(__dirname, "./images"), {
+    root: "images"
+}));
+
+
+const router = new Router();
+
+router.get('/', async (ctx) => {
+    ctx.body = await new Promise((resolve, reject) => {
+        fs.readFile(path.resolve(__dirname, "./图片懒加载.html"), "utf-8", (err, res) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(res.toString());
+        });
+    })
+});
+
+router.get("/images/:name", async ctx => {
+    ctx.body = await new Promise((resolve, reject) => {
+        fs.readFile(path.resolve(__dirname, `./images/${ctx.params.name}`), (err, res) => {
+            if (err) {
+                reject(err)
+            }
+            setTimeout(() => {
+
+                resolve(res);
+            }, 1000)
+        });
+    })
+})
+
+app.use(router.routes());
+
+app.listen(3000);
+
+console.log('Server running on port 3000');
