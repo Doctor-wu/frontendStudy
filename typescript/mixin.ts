4ec215cbeed6e87dc80c2f06ea93597972a27783
Doctorwu <wuzhouchun@qq.com>
2020-12-06 12:54:48 +0800

    feat(TS): 类型变换

--- typescript/mixin.ts
+++ typescript/mixin.ts
@@ -4,10 +4,16 @@ interface AnyObject{
 
 function mixin<T, U>(one: T, two:U):T&U{
     const result = <(T&U)>{};
-    for(let key in one)
-        (<T>result)[key] = one[key];
-    for(let key in two)
-        (<U>result)[key] = two[key];
+    for(let key in one) {
+        if((one as Object).hasOwnProperty(key)){
+            (<T>result)[key] = one[key];
+        }
+    }
+    for(let key in two){
+        if((two as Object).hasOwnProperty(key)){
+            (<U>result)[key] = two[key];
+        }
+    }
 
     return result;
 }
