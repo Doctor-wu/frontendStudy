be2569c6efb971f29230fb3daeb88bdf01929680
Doctorwu <wuzhouchun@qq.com>
2021-02-23 16:29:00 +0800

    HTTP2

--- learn-vue-deeply/src/store/index.js
+++ learn-vue-deeply/src/store/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 Vue.use(Vuex)
-const modulesFiles = require.context('./modules', true, /\.js$/)
+const modulesFiles = require.context('./modules', true, /\.js$/);
+console.log(modulesFiles);
 const mymodules = modulesFiles.keys().reduce((modules, modulePath) => {
+    console.log(modulePath, modules);
     const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
     const value = modulesFiles(modulePath)
     modules[moduleName] = value.default
